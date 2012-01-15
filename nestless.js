c6e82a986e38914c1bd651fd2421fc16c1dbd541
Lal'C Mellk Mal <lalc@doushio.com>
2012-01-15 01:53:34 -0800

    Stub analysis pass

--- nestless.js
+++ nestless.js
@@ -9,19 +9,112 @@ var tokenIds = Narcissus.definitions.tokenIds;
 eval(Narcissus.definitions.consts);
 const CALLBACK_RE = /c(?:all)?b(?:ack)?/i;
 
-// Global state
+var OPTS = {};
+
+/* ANALYSIS */
+
+function analysis() {
+
+function analyzeFunc(node) {
+	var script = node.body;
+	if (script.type == GENERATOR)
+		script = script.body;
+	if (script.type != SCRIPT)
+		throw new Nope("Unexpected in function form", node);
+	analyzeStmts(script.children);
+}
+
+var analyzer = {
+	expr: expr,
+	func: analyzeFunc,
+	yieldExpr: function (node) {},
+};
+
+function block(node) {
+	if (node.type != BLOCK)
+		throw new Nope("That's no block!", node);
+	analyzeStmts(node.children);
+}
+
+function analyzeStmt(node) {
+	var stmt = analyzeStmt;
+	switch (node.type) {
+	case BLOCK:
+		block(node);
+		break;
+	case IF:
+		stmt(node.thenPart);
+		if (node.elsePart)
+			stmt(node.elsePart);
+		break;
+	case DO:
+	case FOR:
+	case WHILE:
+		// ignore conditions etc.
+		if (node.body.type == BLOCK)
+			block(node.body);
+		else
+			stmt(node.body);
+		break;
+	case SWITCH:
+		node.cases.forEach(function (casa) {
+			block(casa.statements);
+		});
+		break;
+	case TRY:
+		block(node.tryBlock);
+		node.catchClauses.forEach(function (clause) {
+			block(clause.block);
+		});
+		if (node.finallyBlock)
+			block(node.finallyBlock);
+		break;
+	case FUNCTION:
+		analyzeFunc(node);
+		break;
+	case BREAK:
+	case CONTINUE:
+		break;
+	case RETURN:
+		if (node.children.length)
+			analyzer.expr(node.children[0]);
+		break;
+	case SEMICOLON:
+		if (!splitArrow(node)) {
+			//if (params.length)
+			//	throw new Nope("Orphaned comma expression", node);
+			analyzer.expr(node.expression);
+			break;
+		}
+		break;
+	case THROW:
+		break;
+	case VAR:
+		node.children.forEach(function (decl) {
+			if (decl.initializer)
+				analyzer.expr(decl.initializer);
+		});
+		break;
+	default:
+		console.error(node);
+		throw new Nope('Unexpected ' + nodeType(node), node);
+	}
+}
+
+function analyzeStmts(nodes) {
+	nodes.forEach(analyzeStmt);
+}
+
+return analyzeStmt;
+}
+
+/* MUTATION */
+
+function mutation() {
+
 var stack = [];
 var replacements = {};
 var insertions = {};
-var OPTS = {};
-
-function nodeType(node) {
-	var type = node.type;
-	for (var name in tokenIds)
-		if (tokenIds[name] === type)
-			return name;
-	return '<unknown node type>';
-}
 
 function replace(start, end, str) {
 	if (!start)
@@ -94,6 +187,236 @@ function mutateFunc(node) {
 		insert(node.end-1, scope.closes.join(''));
 }
 
+var mutator = {
+	expr: expr,
+	func: mutateFunc,
+	yieldExpr: function yieldExpr(node) {
+		if (!stack.length)
+			throw new Nope("Can't yield in global scope", node);
+		var scope = stack[0];
+		if (scope.callback && scope.canYield) {
+			replace(node.start, node.start+6, scope.callback+'(null, ');
+			insert(node.value.end, ')');
+		}
+		else
+			throw new Nope("Can't yield in non-bound scope", node);
+	},
+};
+
+function stmt(node) {
+	var scope = stack[0];
+
+	switch (node.type) {
+	case BLOCK:
+		block(node);
+		break;
+	case IF:
+		stmt(node.thenPart);
+		if (node.elsePart)
+			stmt(node.elsePart);
+		break;
+	case DO:
+	case FOR:
+	case WHILE:
+		if (node.body.type == BLOCK)
+			block(node.body, {canBreakContinue: true});
+		else
+			stmt(node.body);
+		break;
+	case SWITCH:
+		node.cases.forEach(function (casa) {
+			block(casa.statements, {canBreakContinue: true, cannotBind: true});
+		});
+		break;
+	case TRY:
+		block(node.tryBlock, {canThrow: false});
+		node.catchClauses.forEach(function (clause) {
+			block(clause.block);
+		});
+		if (node.finallyBlock)
+			block(node.finallyBlock);
+		break;
+
+	case FUNCTION:
+		mutateFunc(node);
+		break;
+
+	case BREAK:
+	case CONTINUE:
+		if (!scope.canBreakContinue)
+			throw new Nope("Can't " + (node.type == BREAK ? "break" : "continue") + " after binding", node);
+		break;
+
+	case RETURN:
+		if (node.children.length)
+			mutator.expr(node.children[0]);
+		if (!stack.length)
+			throw new Nope("Can't return in global scope", node);
+		if (scope.callback && scope.canYield) {
+			replace(node.start, node.start+7, 'return '+scope.callback+'(null, ');
+			insert(node.value.end, ')');
+			scope.returnAfter = true;
+		}
+		break;
+
+	case SEMICOLON:
+		var arrow = splitArrow(node);
+		if (!arrow) {
+			mutator.expr(node.expression);
+			break;
+		}
+		if (!stack.length)
+			throw new Nope("Can't bind in global scope; please wrap in {}s", node);
+		if (scope.cannotBind)
+			throw new Nope("Can't bind in a switch; please wrap in {}s", node);
+		var cb = scope.callback;
+		if (!cb)
+			throw new Nope("Can't bind inside a function without a callback parameter", node);
+		scope.canYield = true;
+		scope.canThrow = true;
+		scope.canBreakContinue = false;
+		replace(node.start, arrow.rhs.start, '');
+		var err = 'err';
+		arrow.params.unshift(err);
+		var newCall = 'function (' + arrow.params.join(', ') + ') { if ('+err+') return '+cb+'('+err;
+		if (arrow.argList.children.length > 0)
+			newCall = ', ' + newCall;
+		replace(arrow.argList.end, node.end, newCall);
+		close('}); ');
+		break;
+	case THROW:
+		if (!stack.length)
+			break;
+		var scope = stack[0];
+		if (scope.callback && stack.canThrow) {
+			replace(node.start, node.start+6, 'return '+scope.callback+'(');
+			insert(node.exception.end, ')');
+		}
+		break;
+	case VAR:
+		node.children.forEach(function (decl) {
+			if (decl.initializer)
+				mutator.expr(decl.initializer);
+		});
+		break;
+	default:
+		console.error(node);
+		throw new Nope('Unexpected ' + nodeType(node), node);
+	}
+}
+
+function stmts(nodes) {
+	var scope = stack[0];
+	for (var i = 0; i < nodes.length; i++) {
+		var node = nodes[i];
+		if (scope.dead)
+			insert(node.start, '/* DEAD */ ');
+		stmt(node);
+		if (scope.returnAfter && i < nodes.length - 1) {
+			scope.returnAfter = false;
+			scope.dead = true;
+		}
+	}
+}
+
+return {stmt: stmt, replacements: replacements, insertions: insertions};
+}
+
+/* OUTPUT */
+
+function dumpPoints(results, src) {
+	var prev = 0;
+	results.points.forEach(function (p) {
+		var color = require('ansi-color');
+		var width = 60, context = 10;
+		var ins = results.insertions[p];
+		var repl = results.replacements[p];
+		var end = repl ? repl.end : p;
+		var result = '';
+		var action = "inserting"
+		var before = " ..." + src.substring(p-context, p).replace(/\s/g, ' ') + '`';
+		var after = "`" + src.substring(end, end+context).replace(/\s/g, ' ') + "...";
+		var quote, rhs = '', quoteColor = 'green';
+		if (repl) {
+			quote = src.substring(p, end);
+			var result = repl.str;
+			if (ins)
+				result = ins.join('') + result;
+			if (result) {
+				quoteColor = 'yellow';
+				action = 'replacing';
+				var space = Math.max(0, width - action.length - before.length - quote.length - after.length - 2);
+				rhs = Array(space+1).join(' ') + "-> '" + color.set(result, quoteColor) + "'";
+			}
+			else {
+				quoteColor = 'red';
+				action = ' deleting';
+			}
+		}
+		else
+			quote = color.set(ins.join(''), 'green');
+		if (p < prev)
+			rhs += color.set(' SKIPPED', 'red');
+		console.error(color.set(action, quoteColor) + before + color.set(quote, quoteColor) + after + rhs);
+		prev = end;
+	});
+}
+
+function emit(src, results, out) {
+	var replacements = results.replacements, insertions = results.insertions;
+	// Build a list of all insertion/replacement indices
+	var points = Object.keys(replacements).map(function (x) {
+		return parseInt(x, 10);
+	});
+	for (var x in insertions)
+		if (!(x in replacements))
+			points.push(parseInt(x, 10));
+	points.sort(function (b, a) { return b - a; });
+
+	if (OPTS.verbose) {
+		results.points = points;
+		dumpPoints(results, src);
+	}
+
+	out = out ? fs.createWriteStream(out) : process.stdout;
+	var pos = 0;
+	while (pos < src.length) {
+		var next = points.length ? points.shift() : src.length;
+		while (next < pos) {
+			var repl = replacements[next];
+			if (repl)
+				throw new Error("Replacement >>>" + repl.str + "<<< was overwritten by another replacement");
+			var ins = insertions[next];
+			if (ins)
+				throw new Error("Insertion >>>" + ins.join('') + "<<< was overwritten by another replacement");
+			next = points.length ? points.shift() : src.length;
+		}
+		out.write(src.substring(pos, next));
+		pos = next;
+		if (next >= src.length)
+			break;
+		var ins = insertions[pos];
+		if (ins)
+			out.write(ins.join(''));
+		var repl = replacements[pos];
+		if (repl) {
+			out.write(repl.str);
+			pos = repl.end;
+		}
+	}
+}
+
+/* HELPERS */
+
+function nodeType(node) {
+	var type = node.type;
+	for (var name in tokenIds)
+		if (tokenIds[name] === type)
+			return name;
+	return '<unknown node type>';
+}
+
+// Generic expression walker
 function expr(node) {
 	var thisExpr = expr.bind(this);
 	switch (node.type) {
@@ -143,240 +466,37 @@ function expr(node) {
 	}
 }
 
-var mutator = {
-	expr: expr,
-	func: mutateFunc,
-	yieldExpr: function yieldExpr(node) {
-		if (!stack.length)
-			throw new Nope("Can't yield in global scope", node);
-		var scope = stack[0];
-		if (scope.callback && scope.canYield) {
-			replace(node.start, node.start+6, scope.callback+'(null, ');
-			insert(node.value.end, ')');
-		}
-		else
-			throw new Nope("Can't yield in non-bound scope", node);
-	},
-};
-
-
-function stmt(node) {
-	var scope = stack[0];
-	switch (node.type) {
-	case BLOCK:
-		block(node);
-		break;
-	case IF:
-		stmt(node.thenPart);
-		if (node.elsePart)
-			stmt(node.elsePart);
-		break;
-	case DO:
-	case FOR:
-	case WHILE:
-		// ignore conditions etc.
-		if (node.body.type == BLOCK)
-			block(node.body, {canBreakContinue: true});
-		else
-			stmt(node.body);
-		break;
-	case SWITCH:
-		node.cases.forEach(function (casa) {
-			block(casa.statements, {canBreakContinue: true, cannotBind: true});
-		});
-		break;
-	case TRY:
-		block(node.tryBlock, {canThrow: false});
-		node.catchClauses.forEach(function (clause) {
-			block(clause.block);
-		});
-		if (node.finallyBlock)
-			block(node.finallyBlock);
-		break;
-
-	case FUNCTION:
-		mutateFunc(node);
-		break;
-
-	case BREAK:
-	case CONTINUE:
-		if (!scope.canBreakContinue)
-			throw new Nope("Can't " + (node.type == BREAK ? "break" : "continue") + " after binding", node);
-		break;
-
-	case RETURN:
-		if (node.children.length)
-			mutator.expr(node.children[0]);
-		if (!stack.length)
-			throw new Nope("Can't return in global scope", node);
-		if (scope.callback && scope.canYield) {
-			replace(node.start, node.start+7, 'return '+scope.callback+'(null, ');
-			insert(node.value.end, ')');
-			scope.returnAfter = true;
-		}
-		break;
-
-	case SEMICOLON:
-		var arrow = node.expression;
-		var params = [];
-		while (arrow.type == COMMA) {
-			var ident = arrow.children[0];
-			if (!ident || ident.type != IDENTIFIER)
-				throw new Nope('Expected identifier', ident);
-			params.push(ident.value);
-			arrow = arrow.children[1];
-		}
-		if (arrow.type != LT) {
-			//if (params.length)
-			//	throw new Nope("Orphaned comma expression", node);
-			mutator.expr(node.expression);
-			break;
-		}
-		if (!stack.length)
-			throw new Nope("Can't bind in global scope; please wrap in {}s", node);
-		if (scope.cannotBind)
-			throw new Nope("Can't bind in a switch; please wrap in {}s", node);
-		var cb = scope.callback;
-		if (!cb)
-			throw new Nope("Can't bind inside a function without a callback parameter", node);
-		scope.canYield = true;
-		scope.canThrow = true;
-		scope.canBreakContinue = false;
-		var minus = arrow.children[1];
-		if (minus.type != UNARY_MINUS)
-			throw new Nope('Incomplete arrow', arrow);
+function splitArrow(node) {
+	var arrow = node.expression;
+	var params = [];
+	var validTuple = true;
+	while (arrow.type == COMMA) {
 		var ident = arrow.children[0];
-		if (ident.type != IDENTIFIER)
-			throw new Nope('Identifier(s) expected before arrow', arrow);
+		if (!ident || ident.type != IDENTIFIER) {
+			valid = false;
+			break;
+		}
 		params.push(ident.value);
-		var rhs = arrow.children[1].children[0];
-		if (rhs.type != CALL)
-			throw new Nope('Call expected after arrow', arrow);
-		var argList = rhs.children[1];
-		if (rhs.children.length != 2 || argList.type != LIST)
-			throw new Nope('Unexpected call format', rhs);
-		var args = argList.children;
-		replace(node.start, rhs.start, '');
-		var err = 'err';
-		params.unshift(err);
-		var newCall = 'function (' + params.join(', ') + ') { if ('+err+') return '+cb+'('+err;
-		if (args.length > 0)
-			newCall = ', ' + newCall;
-		replace(argList.end, node.end, newCall);
-		close('}); ');
-		break;
-	case THROW:
-		if (!stack.length)
-			break;
-		var scope = stack[0];
-		if (scope.callback && stack.canThrow) {
-			replace(node.start, node.start+6, 'return '+scope.callback+'(');
-			insert(node.exception.end, ')');
-		}
-		break;
-	case VAR:
-		node.children.forEach(function (decl) {
-			if (decl.initializer)
-				mutator.expr(decl.initializer);
-		});
-		break;
-	default:
-		console.error(node);
-		throw new Nope('Unexpected ' + nodeType(node), node);
+		arrow = arrow.children[1];
 	}
-}
-
-function stmts(nodes) {
-	var scope = stack[0];
-	for (var i = 0; i < nodes.length; i++) {
-		var node = nodes[i];
-		if (scope.dead)
-			insert(node.start, '/* DEAD */ ');
-		stmt(node);
-		if (scope.returnAfter && i < nodes.length - 1) {
-			scope.returnAfter = false;
-			scope.dead = true;
-		}
-	}
-}
-
-function dumpPoints(points, src) {
-	var prev = 0;
-	points.forEach(function (p) {
-		var color = require('ansi-color');
-		var width = 60, context = 10;
-		var ins = insertions[p];
-		var repl = replacements[p];
-		var end = repl ? repl.end : p;
-		var result = '';
-		var action = "inserting"
-		var before = " ..." + src.substring(p-context, p).replace(/\s/g, ' ') + '`';
-		var after = "`" + src.substring(end, end+context).replace(/\s/g, ' ') + "...";
-		var quote, rhs = '', quoteColor = 'green';
-		if (repl) {
-			quote = src.substring(p, end);
-			var result = repl.str;
-			if (ins)
-				result = ins.join('') + result;
-			if (result) {
-				quoteColor = 'yellow';
-				action = 'replacing';
-				var space = Math.max(0, width - action.length - before.length - quote.length - after.length - 2);
-				rhs = Array(space+1).join(' ') + "-> '" + color.set(result, quoteColor) + "'";
-			}
-			else {
-				quoteColor = 'red';
-				action = ' deleting';
-			}
-		}
-		else
-			quote = color.set(ins.join(''), 'green');
-		if (p < prev)
-			rhs += color.set(' SKIPPED', 'red');
-		console.error(color.set(action, quoteColor) + before + color.set(quote, quoteColor) + after + rhs);
-		prev = end;
-	});
-}
-
-function emit(src, out) {
-	// Build a list of all insertion/replacement indices
-	var points = Object.keys(replacements).map(function (x) {
-		return parseInt(x, 10);
-	});
-	for (var x in insertions)
-		if (!(x in replacements))
-			points.push(parseInt(x, 10));
-	points.sort(function (b, a) { return b - a; });
-
-	if (OPTS.verbose)
-		dumpPoints(points, src);
-
-	out = out ? fs.createWriteStream(out) : process.stdout;
-	var pos = 0;
-	while (pos < src.length) {
-		var next = points.length ? points.shift() : src.length;
-		while (next < pos) {
-			var repl = replacements[next];
-			if (repl)
-				throw new Error("Replacement >>>" + repl.str + "<<< was overwritten by another replacement");
-			var ins = insertions[next];
-			if (ins)
-				throw new Error("Insertion >>>" + ins.join('') + "<<< was overwritten by another replacement");
-			next = points.length ? points.shift() : src.length;
-		}
-		out.write(src.substring(pos, next));
-		pos = next;
-		if (next >= src.length)
-			break;
-		var ins = insertions[pos];
-		if (ins)
-			out.write(ins.join(''));
-		var repl = replacements[pos];
-		if (repl) {
-			out.write(repl.str);
-			pos = repl.end;
-		}
-	}
+	if (arrow.type != LT)
+		return false;
+	if (!validTuple)
+		throw new Nope('Identifiers(s) expected in tuple before arrow', ident);
+	var minus = arrow.children[1];
+	if (minus.type != UNARY_MINUS)
+		throw new Nope('Incomplete arrow', arrow);
+	var ident = arrow.children[0];
+	if (ident.type != IDENTIFIER)
+		throw new Nope('Identifier(s) expected before arrow', arrow);
+	params.push(ident.value);
+	var rhs = arrow.children[1].children[0];
+	if (rhs.type != CALL)
+		throw new Nope('Call expected after arrow', arrow);
+	var argList = rhs.children[1];
+	if (rhs.children.length != 2 || argList.type != LIST)
+		throw new Nope('Unexpected call format', rhs);
+	return {rhs: rhs, argList: argList, params: params};
 }
 
 function Nope(message, node, end) {
@@ -387,14 +507,22 @@ function Nope(message, node, end) {
 }
 util.inherits(Nope, Error);
 
+/* MAIN */
+
 function flatten(src, filename, outputFilename) {
 	var root = parser.parse(src, filename);
 	/* clear out the damn tokenizer for debugging */
 	for (var k in root.tokenizer)
 		delete root.tokenizer[k];
+
+	var results;
 	try {
-		// Don't want to create a global scope
-		root.children.forEach(stmt);
+		var analyzeStmt = analysis();
+		root.children.forEach(analyzeStmt);
+
+		results = mutation();
+		var mutateStmt = results.stmt;
+		root.children.forEach(mutateStmt);
 	}
 	catch (e) {
 		if (!(e instanceof Nope))
@@ -409,7 +537,8 @@ function flatten(src, filename, outputFilename) {
 		}
 		throw e;
 	}
-	emit(src, outputFilename);
+
+	emit(src, results, outputFilename);
 }
 exports.flatten = flatten;
 
